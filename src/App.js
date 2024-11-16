0dec3d89d552282e88a462cb3321cb0f373b42cc
RajWalke <rajwalke66@gmail.com>
2024-11-16 12:24:21 +0530

    Initail commit for project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+const App=()=> {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p className='text-2xl text-red-400'>
+         Hello Lets build moviebox
+        </p>
+      </header>
+    </div>
+  );
+}
+ 
+export default App;
